b8d7b9f7340a541210c9fc93b130104d66c6159a
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2025-02-02 14:37:10 +0530

    Important : interesting problem : sort and order problem

--- src/components/DisplayTodo.tsx
+++ src/components/DisplayTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/DisplayTodo.css'
 import log from '../debugging/debug';
 import { SingleTodoObject } from '../model/SingleTodoModel';
@@ -11,17 +11,33 @@ interface SingleTodoProps {
 }
 
 const DisplayTodo: React.FC<SingleTodoProps> = (props) => {
-    const a = 't';
-
+    const [checkEdit, setCheckEdit] = useState<boolean>(false)
+    const [editValue, setEditvalue] = useState<string>(props.singleTodo.text)
     const handleTodoEdits = (id: number) => {
+        setCheckEdit(checkEdit ? false : true)
         log("edit", id)
+    }
+    const handleOnChangeEdit = (e: React.ChangeEvent<HTMLInputElement>)=>{
+        log("ok")
+        setEditvalue(e.target.value)
+
     }
     return (
         <div className={`display__todo ${process.env.VITE_NODE_ENV ? 'dev-mode' : ''} ${props.singleTodo.isDone ? 'completed__todo' : ''}`}>
             {/* <strong>here</strong> */}
             <div className={`display__todo__single `}>
                 {/* {props.id} */}
-                {props.singleTodo.text}
+                {checkEdit ?
+                    <input
+                        value={editValue}
+                        onChange={(e)=>handleOnChangeEdit(e)}
+                    />
+
+                    // </input>
+                    :
+                    <span>{props.singleTodo.text}</span>
+                }
+                {/* {props.singleTodo.text} */}
                 {process.env.VITE_NODE_ENV ?
                     <s>
                         {props.singleTodo.id}
